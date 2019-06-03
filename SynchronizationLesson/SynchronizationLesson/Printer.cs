170adbf66a0ea2b1974c1676e90ec624bb8aa664
Аслан Нурсеитов <grooves97@gmail.com>
2019-06-03 10:35:40 +0600

    Синхронизация потоков  lockObject/Interlocked.Increment/Monitor.Enter

---
+++ SynchronizationLesson/SynchronizationLesson/Printer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Remoting.Contexts;
+
+namespace SynchronizationLesson
+{
+    [Synchronization]
+    public class Printer : ContextBoundObject
+    {
+        private int _number = 0;
+        private object lockObject = new object();
+
+        public void Print()
+        {
+            Interlocked.Increment(ref _number);
+
+            Monitor.Enter(lockObject);
+            try
+            {
+                var curentThread = Thread.CurrentThread;
+                Console.WriteLine($"{curentThread.Name} начинает работать");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(5 * new Random().Next(100));
+                    Console.Write(i + " " + "(" + _number + ")");
+                    _number = curentThread.ManagedThreadId;
+                }
+
+                Console.WriteLine($"\n{curentThread.Name} закончил работу");
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+            }
+        }
+    }
+}
+
+
+            /*lock (lockObject)
+            {
+                var curentThread = Thread.CurrentThread;
+Console.WriteLine($"{curentThread.Name} начинает работать");
+
+                for (int i = 0; i< 10; i++)
+                {
+                    Thread.Sleep(5 * new Random().Next(100));
+                    Console.Write(i + " " + "(" + _number + ")");
+                    _number = curentThread.ManagedThreadId;
+                }
+
+                Console.WriteLine($"\n{curentThread.Name} закончил работу");
+
+            }*/
